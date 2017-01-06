f94bcc1e9e6923b4db61062b275fc478bf43fce5
Maciej Laciak <maciej.laciak@gmail.com>
2017-01-06 16:55:43 +0000

    Implements the postcode validator to accept the UK's 6 standard postcode formats.

--- src/main/java/io/mstream/addresses/api/validation/PostcodeValidator.java
+++ src/main/java/io/mstream/addresses/api/validation/PostcodeValidator.java
@@ -1,13 +1,61 @@
 package io.mstream.addresses.api.validation;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
+
 
 @Component
 public class PostcodeValidator implements Validator<String> {
 
+    private static final Set<String> POSTCODE_FORMATS = new HashSet<>(
+            Arrays.asList(
+                    "AA0A0AA",
+                    "A0A0AA",
+                    "A00AA",
+                    "A000AA",
+                    "AA00AA",
+                    "AA000AA"
+            )
+    );
+
+    private static final ValidationResult validationFailureResult =
+            ValidationResult.fromViolations(
+                    Collections.singleton(
+                            format(
+                                    "Invalid postcode format. Supported formats: %s",
+                                    POSTCODE_FORMATS
+                            )
+                    )
+            );
+
+    private final Pattern postcodePattern;
+
+    @Autowired
+    public PostcodeValidator() {
+        String postcodeFormatsRegex = POSTCODE_FORMATS
+                .stream()
+                .map(format -> format
+                        .replaceAll("A", "[A-Z]")
+                        .replaceAll("0", "\\\\d"))
+                .collect(Collectors.joining("|"));
+        postcodePattern = Pattern.compile(postcodeFormatsRegex);
+    }
+
     @Override
-    public ValidationResult validate(String object) {
-        return ValidationResult.noViolations();
+    public ValidationResult validate(String postcode) {
+        boolean matches = postcodePattern.matcher(postcode).matches();
+        return matches ?
+                ValidationResult.noViolations() :
+                validationFailureResult;
     }
 }
