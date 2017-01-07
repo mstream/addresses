d7ffd3797009d4633e19bcd4d4669b68d2684fe5
Maciej Laciak <maciej.laciak@gmail.com>
2017-01-07 17:19:01 +0000

    Adds a springboot gradle project template.

---
+++ src/main/java/io/mstream/addresses/AddressesApplication.java
@@ -0,0 +1,12 @@
+package io.mstream.addresses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressesApplication.class, args);
+	}
+}
---
+++ src/test/java/io/mstream/addresses/AddressesApplicationTests.java
@@ -0,0 +1,16 @@
+package io.mstream.addresses;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AddressesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
